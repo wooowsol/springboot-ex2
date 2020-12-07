06e61e1501ae292436ff469a1e4531d47b799742
Eunsol Park <71619744+wooowsol@users.noreply.github.com>
2020-12-07 18:22:50 +0900

    테스트 코드를 통한 CRUD 연습

---
+++ src/main/java/org/zerock/ex2/Servletlnitializer.java
@@ -0,0 +1,4 @@
+package org.zerock.ex2;
+
+public class Servletlnitializer {
+}
---
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -0,0 +1,80 @@
+package org.zerock.ex2.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.ex2.domain.Memo;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Test
+    public void testClass() {
+
+        System.out.println(memoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsertDummies() {
+
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("Sample..." + i).build();
+            memoRepository.save(memo);
+        });
+    }
+
+    @Test
+    public void testSelect() {
+        //데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("================================");
+
+        if (result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+
+
+    }
+
+    @Transactional
+    @Test
+    public void testSelect2() {
+        //데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Memo memo = memoRepository.getOne(mno);
+
+        System.out.println("================================");
+
+        System.out.println(memo);
+    }
+
+    @Test
+    public void testUpdate(){
+
+        Memo memo = Memo.builder().mno(100L).memoText("UPdate Text").build();
+
+        System.out.println(memoRepository.save(memo));
+    }
+
+    @Test
+    public void testDelete(){
+
+        Long mno = 100L;
+
+        memoRepository.deleteById(mno);
+
+    }
+
+}
